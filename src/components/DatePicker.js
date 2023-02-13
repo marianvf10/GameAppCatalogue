a9f052412139660bedd4980bb59f75f142cf684f
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-02-13 20:55:38 -0300

    datepicker funcionality added

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,43 +1,53 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
 import React from "react";
 import { useState } from "react";
-import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { TextInput } from "react-native-gesture-handler";
 
 const MyDatePicker = (onChangeDate) => {
-  const [date, setDate] = useState(new Date());
+  
+  const [date,setDate] = useState(new Date());
+  const [mode,setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [text,setText] = useState('empty');
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
+  const onChange = (event,selectedDate) =>{
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
     setDate(currentDate);
-  };
 
+    let tempDate = new Date(currentDate);
+    let fDate = tempDate.getDate() + '/' + (tempDate.getMonth()+1) + '/' + tempDate.getFullYear();
+
+    setText(fDate);
+
+    
+
+
+  }
   const showMode = (currentMode) => {
-    DateTimePickerAndroid.open({
-      value: date,
-      onChange,
-      mode: date,
-      display: "calendar",
-    });
-  };
-
-  onFocus = () => {
-    showDatepicker();
-  };
-
-  const showDatepicker = () => {
-    showMode("date");
-  };
-
+    setShow(true);
+    setMode(currentMode);
+  }
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.inputContainer}
-        placeholder="choose a date"
-        onFocus={onFocus}
-        value={date.toDateString()}
-        onChangeText={(value) =>  console.log(value)}
-      />
+      <Text style = {{fontWeight:'bold', fontSize: 20}}>{text}</Text>
+      <View style = {{margin:20}}>
+        <Button title='Ingresar fecha estreno' onPress={() =>{showMode('date')}}/>
+        <Button title ='click child' onClick={()=> {onChangeDate(date)}}/>
+       
+      </View>
+
+      {show && (
+        <DateTimePicker
+        testID='dateTimePicker'
+        value={date}
+        mode={mode}
+        display='default'
+        is24Hour={true}
+        onChange={onChange}
+        />
+      )}
     </View>
   );
 };
@@ -46,21 +56,9 @@ export default MyDatePicker;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.5,
-    backgroundColor: "#fff",
-    alignItems: "center",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "700",
-  },
-  inputContainer: {
-    minWidth: "90%",
-    padding: 13,
-    marginVertical: 6,
-    borderWidth: 1,
-    borderColor: "#ddd",
-    borderRadius: 6,
-    margin: 12,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
