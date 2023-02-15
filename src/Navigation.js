dc2b69cf04187575c418f991da42084a2eeb493f
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-02-15 11:03:51 -0300

    game details and list added on navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, StackNavigator } from "@react-navigation/native";
 import Home from "./screens/Home";
 import Add from "./screens/Add";
+import GameList from "./screens/GameList";
+import GameDetails from "./screens/GameDetails";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +17,16 @@ return(
     <Stack.Screen 
     name = "Add" 
     component={Add}
+    options={{presentation:'modal'}}
+    />
+    <Stack.Screen 
+    name = "GameList" 
+    component={GameList}
+    options={{presentation:'modal'}}
+    />
+    <Stack.Screen 
+    name = "GameDetails" 
+    component={GameDetails}
     options={{presentation:'modal'}}
     />
 </Stack.Navigator>
