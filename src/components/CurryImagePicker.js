f8c4ab68115d478224f1d44e003ebd96bb5f76e8
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-02-22 19:03:07 -0300

    small changes

--- src/components/CurryImagePicker.js
+++ src/components/CurryImagePicker.js
@@ -14,15 +14,13 @@ const CurryImagePicker = () => {
     const pickImageAsync = async () => {
       let result = await ImagePicker.launchImageLibraryAsync({
         allowsEditing: true,
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
         aspect: [4, 3],
         quality: 1,
       });
-      if (!result.canceled) {
+    
         setSelectedImage(result.assets[0].uri);
-      } else {
-        alert('You did not select any image.');
-      }
+      
       
     };
   
