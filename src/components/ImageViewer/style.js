90419dd121ce7e4c5d1a9ce81d2bd09e6c93ae13
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-02-24 15:12:44 -0300

    style image added

--- src/components/ImageViewer/style.js
+++ src/components/ImageViewer/style.js
@@ -1,19 +1,7 @@
 import { StyleSheet } from "react-native"
 export const styles = StyleSheet.create({
-    container:{
-        width: '100%',
-        alignItems: 'center',
-        
-    },
-    imageContainer:{
-        borderWidth:1,
-        borderColor: 'black',
-        width:'80%',
-        height: 180,
-        backgroundColor:'#eee',
-    },
-    button:{
-        margin:8
+    image:{
+        width:'100%',
+        height:'100%'
     }
-   
 })
