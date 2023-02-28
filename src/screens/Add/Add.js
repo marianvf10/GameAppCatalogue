728d92e2c3f73847a22bda4d34e7c53e46465b2d
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-02-28 17:43:24 -0300

    add screen completed

--- src/screens/Add/Add.js
+++ src/screens/Add/Add.js
@@ -1,12 +1,8 @@
 import { useNavigation } from "@react-navigation/native";
-import { useContext, useState, useLayoutEffect } from "react";
-import {firebase} from '../../../config/firebase'
+import { useState, useLayoutEffect } from "react";
+import { firebase } from "../../../config/firebase";
 import React from "react";
-import {
-  Text,
-  View,
-  Button, TouchableOpacity
-} from "react-native";
+import { Text, View, Button, TouchableOpacity } from "react-native";
 import { ScrollView, TextInput } from "react-native-gesture-handler";
 import MyDatePicker from "../../components/DatePicker/DatePicker";
 import { database } from "../../../config/firebase";
@@ -14,12 +10,11 @@ import { collection, addDoc } from "firebase/firestore";
 import CurryImagePicker from "../../components/CurryImagePicker/CurryImagePicker";
 import { styles } from "./style";
 
-
 export default function Add() {
-
   const navigation = useNavigation();
 
-  const [newItem, setNewItem] = useState({
+  //este objeto es el que se guarda en la bd
+  const obj = {
     name: "",
     platform: "",
     price: 0,
@@ -27,44 +22,58 @@ export default function Add() {
     createAt: new Date(),
     releaseDate: "",
     imageUri: "",
+  };
+
+  //este es el estado del objeto que se tiene que guardar, estos atributos se reciben del usuario
+  const [newItem, setNewItem] = useState({
+    name: "",
+    platform: "",
+    price: 0,
+    genre: "",
+    releaseDate: "",
   });
 
-  const [addD, setNewDate] = useState('');
+  const [addD, setNewDate] = useState("");
   const [selectedImage, setNewImage] = useState(null);
 
   //con esta funcion agrego un nuevo documento a la bd
   const onSend = async () => {
-
-    uploadImage();
-    await addDoc(collection(database, "games"), newItem);
+    const url = await uploadImage();
+    addAttribute(url);
+    await addDoc(collection(database, "games"), obj);
     navigation.goBack();
-    
   };
 
-
+  const addAttribute = (downloadUrl) => {
+    obj.name = newItem.name;
+    obj.platform = newItem.platform;
+    obj.price = newItem.price;
+    obj.genre = newItem.genre;
+    obj.releaseDate = newItem.releaseDate;
+    obj.imageUri = downloadUrl;
+  };
 
   //con esta funcion recibo informacion del DatePicker
   const addDate = (newDate) => {
     setNewDate(newDate.toString());
     setNewItem({ ...newItem, releaseDate: newDate });
-  }
-
+  };
 
   //con esta funcion recibo informacion del CurryImagePicker
   const addImage = (newImage) => {
     setNewImage(newImage);
-  }
-
+  };
 
   //funcion para subir la imagen a Firebase storage y obtener una URL de descarga
-  const uploadImage = async()=> {
-    const fileName = selectedImage.substring(selectedImage.lastIndexOf('/')+1);
-    
+  const uploadImage = async () => {
+    const fileName = selectedImage.substring(
+      selectedImage.lastIndexOf("/") + 1
+    );
+
     const response = await fetch(selectedImage);
     const blob = await response.blob();
-    var ref =  firebase.storage().ref(`games/images/${fileName}`);
-    
-    
+    var ref = firebase.storage().ref(`games/images/${fileName}`);
+
     try {
       //subo la imagen
       await ref.put(blob);
@@ -72,17 +81,12 @@ export default function Add() {
       console.log(error);
     }
     try {
-      //descargo la imagen y guardo el link en el objeto
-      const setUrl = await ref.getDownloadURL();
-      //con esta linea agrego la url al objeto
-      setNewItem({ ...newItem, imageUri: setUrl });
-      
-      
+      //descargo la imagen y retorno el link de descarga
+      return await ref.getDownloadURL();
     } catch (error) {
       console.log(error);
     }
-  }
-    
+  };
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -98,7 +102,7 @@ export default function Add() {
       contentContainerStyle={{ alignItems: "center" }}
     >
       <Text style={styles.title}>Add a New Game</Text>
-      <CurryImagePicker addImage = {addImage}/>
+      <CurryImagePicker addImage={addImage} />
       <TextInput
         style={styles.inputContainer}
         placeholder="Product Name"
@@ -120,10 +124,9 @@ export default function Add() {
         placeholder="Genre"
         onChangeText={(text) => setNewItem({ ...newItem, genre: text })}
       />
-      <MyDatePicker addDate={addDate}/>
+      <MyDatePicker addDate={addDate} />
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} 
-        onPress={onSend}>
+        <TouchableOpacity style={styles.button} onPress={onSend}>
           <Text style={styles.text}>Subir</Text>
         </TouchableOpacity>
       </View>
