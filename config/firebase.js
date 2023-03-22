e57c665e60e77b4f1dfead925a53318328f91f62
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-03-22 19:15:29 -0300

    project id field solved

--- config/firebase.js
+++ config/firebase.js
@@ -7,10 +7,10 @@ import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
+  authDomain: AUTH_DOMAIN ,
+  projectId: PROJECT_ID ,
+  storageBucket: STORAGE_BUCKET ,
+  messagingSenderId: MESSAGING_SENDER_ID ,
   appId: APP_ID
 };
 
