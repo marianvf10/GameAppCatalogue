aa0f8759a23f82a2d5513a8fdd738e34bb725cd8
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-03-22 17:13:58 -0300

    firebase changes

--- config/firebase.js
+++ config/firebase.js
@@ -1,18 +1,17 @@
+import {API_KEY,AUTH_DOMAIN,PROJECT_ID,STORAGE_BUCKET,MESSAGING_SENDER_ID,APP_ID} from '@env'
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
-import { initializeApp } from "firebase/compat/app";
-
 import 'firebase/storage';
 import {getFirestore} from 'firebase/firestore'
-import Constants from "expo-constants";
+
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
 };
 
 // Initialize Firebase
