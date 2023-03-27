8b7bcfe4a832e5022aa01a9302205cb462344ba6
mariano <mariano.vergara@est.fi.uncoma.edu.ar>
2023-03-27 19:01:12 -0300

    a few comments added

--- src/screens/GameList/GameList.js
+++ src/screens/GameList/GameList.js
@@ -15,30 +15,33 @@ import { fetchGames, fetchMoreGames} from "../../services/games";
 
 export default function GameList() {
   const goDetails = (name, imageUri, price, platform, releaseDate, genre) => {
+    //function to go to details screen
     navigation.navigate("GameDetails", { name, imageUri, price, platform, releaseDate, genre });
   }
   const navigation = useNavigation();
 
-  const [games, setGames] = useState([]); 
+  const [games, setGames] = useState([]); //array where store games
   const [emptyStore, setEmptyStore] = useState(true); //Marca si ya hay juegos registrados en la tienda
-  const [gamesPerLoad] = useState(5); 
-  const [startAfter,setStartAfter] = useState(Object);
-  const [lastGame,setLastGame] = useState(false);
+  const [gamesPerLoad] = useState(5); //to choose how many documents fetch from db 
+  const [startAfter,setStartAfter] = useState(Object); //use for set a start from "x game"
+  const [lastGame,setLastGame] = useState(false); 
 
   useEffect(() => {
-    getPosts();
+    getGames;
   }, []);
 
-  async function getPosts() {
+  async function getGames() {
+    //use the fetch function from service to communicate with db
     const gamesData = await fetchGames(gamesPerLoad,"price");
-    setGames([...games, ...gamesData.posts]);
-    setStartAfter(gamesData.lastVisible);
+    setGames([...games, ...gamesData.posts]); //store games 
+    setStartAfter(gamesData.lastVisible); //set the start game to call in the next fetch 
     if (gamesData.posts.length !=0){
       setEmptyStore(false);
     }
   }
 
   async function getMoreGames() {
+    //same behavior that getGames, just add a new validation to stop scrolling when catch the last game
     if (!lastGame) {
       const gamesData = await fetchMoreGames("price",startAfter,gamesPerLoad);
       setGames([...games, ...gamesData.posts]);
